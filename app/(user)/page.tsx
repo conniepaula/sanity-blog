8d3d2d7fb9b8f9be7b2f974a2954aac41feff447
Connie <conniedpaula@gmail.com>
2023-01-11 19:42:21 -0300

    Add sanity preview

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import PreviewSuspense from "../../components/PreviewSuspense";
 
-function page() {
-  return (
-    <div>
-        <h1 className="text-4xl text-pink-600">Page</h1>
-    </div>
-  )
+const query = groq`
+*[_type=='post'] {
+  ...,
+  author ->,
+  categories[] ->
+} | order(_createdAt desc)`;
+
+export default async function page() {
+  if (previewData()) {
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role="status">
+            <p className="text-center text-lg animate-pulse">
+              Loading Preview Data...
+            </p>
+          </div>
+        }
+      >
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    );
+  }
+  const posts = await client.fetch(query);
+  console.log(posts);
+  return <div>Not in Preview mode</div>;
 }
-
-export default page
